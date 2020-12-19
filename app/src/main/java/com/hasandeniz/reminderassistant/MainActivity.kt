fbc11436853b0a5e3958db97bdb7138da44957c4
hasandeniz <hasandeniz394@gmail.com>
2020-12-19 19:03:18 +0300

    empty project

--- app/src/main/java/com/hasandeniz/reminderassistant/MainActivity.kt
+++ app/src/main/java/com/hasandeniz/reminderassistant/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //hasan first
+        //hasan first 1
     }
 }
 //ALi was here
