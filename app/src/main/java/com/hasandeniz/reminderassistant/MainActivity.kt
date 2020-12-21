e24e5dbe787b05d8f98933e566f12dbbedaea316
hasandeniz <hasandeniz394@gmail.com>
2020-12-21 11:09:15 +0300

    updated to view binding

--- app/src/main/java/com/hasandeniz/reminderassistant/MainActivity.kt
+++ app/src/main/java/com/hasandeniz/reminderassistant/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hasandeniz.reminderassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
     }
 }
