6cabf435f382bb2f45724609e7612f4813d486f5
hasandeniz <hasandeniz394@gmail.com>
2020-12-24 14:28:31 +0300

    recyclerview added all fragments

--- app/src/main/java/com/hasandeniz/reminderassistant/fragments/SundayFragment.kt
+++ app/src/main/java/com/hasandeniz/reminderassistant/fragments/SundayFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hasandeniz.reminderassistant.ExampleItem
 import com.hasandeniz.reminderassistant.R
+import com.hasandeniz.reminderassistant.adapters.RecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_saturday.*
+import kotlinx.android.synthetic.main.fragment_sunday.*
 
 
 class SundayFragment : Fragment() {
@@ -20,6 +25,26 @@ class SundayFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sunday, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val exampleItem = denemeItem()
+        val exampleItem2 = denemeItem()
+
+        val exampleItemList = ArrayList<ExampleItem>()
+        exampleItemList.add(exampleItem)
+        exampleItemList.add(exampleItem2)
+        recyclerViewSunday.adapter = RecyclerViewAdapter(exampleItemList)
+        recyclerViewSunday.layoutManager = LinearLayoutManager(activity)
+        recyclerViewSunday.setHasFixedSize(true)
+
+    }
+
+    private fun denemeItem(): ExampleItem {
+        val text2 = "Deneme 2"
+        val text1 = "Deneme 1"
+        val imageText1 = "12:00"
+        val imageText2 = "13:00"
+        return ExampleItem(text1, text2, imageText1, imageText2)
     }
 
 }
